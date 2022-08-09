5ba8fc998e57c0368d40b4beed8869007652e06f
Md. Nur-A-Redwan-Ul-Azam <azam13bh@outlook.com>
2022-08-09 23:34:16 +0600

    Add project files.

---
+++ StudentRegForm/StudentUI.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentUI.aspx.cs" Inherits="StudentRegForm.StudentUI" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<form id="form2" runat="server" method="Get">
+<div>
+<fieldset>
+<legend>Student Registration Form </legend>
+<table>
+<tr>
+<td>
+<asp:Label ID="Label1" runat="server" Text="Name : ">
+</asp:Label>
+<asp:Label ID="Label2" runat="server" Text=" *"
+ForeColor="red"></asp:Label>
+</td>
+<td>
+<asp:TextBox ID="nameTextBox" autofocus
+required placeholder="Your Name" runat="server"
+ToolTip="Please enter your name" ></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td>
+<asp:Label ID="Label3" runat="server"
+Text="Registration Number: "></asp:Label>
+<asp:Label ID="Label4" runat="server" Text=" *"
+ForeColor="red"></asp:Label>
+</td>
+   
+<td>
+<asp:TextBox ID="regTextBox" required
+runat="server" placeholder="Registration Number"
+ToolTip="Please enter your registration number"
+></asp:TextBox>
+</td>
+</tr>
+<tr>
+<td></td>
+<td>
+<asp:Button ID="saveButton" runat="server" Text="Save" ToolTip="Save" ForeColor="Blue" OnClick="saveButton_Click" />
+</td>
+</tr>
+<tr>
+<td>
+<asp:Label ID="messageLabel" runat="server"
+ForeColor="Gray"></asp:Label>
+
+</td>
+</tr>
+</table>
+</fieldset>
+</div>
+</form>
+</body>
+</html>
---
+++ StudentRegForm/StudentUI.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegForm
+{
+    public partial class StudentUI : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void saveButton_Click(object sender, EventArgs e)
+        {
+            //Connection String
+            string connString = "Data Source=(localdb)\\mssqllocaldb; Initial Catalog=Test; Integrated Security=SSPI;";
+            //Sql Connection
+            SqlConnection sqlConnection = new SqlConnection(connString);
+            //Query
+            string query = "INSERT INTO t_Student(name,regNo) VALUES(@name, @regNo)";
+            //Sql Command
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.AddWithValue("@name", nameTextBox.Text.Trim());
+            sqlCommand.Parameters.AddWithValue("@regNo", regTextBox.Text.Trim());
+
+
+            //Connection open
+            sqlConnection.Open();
+            //Command Execute
+            int rowsEffected = sqlCommand.ExecuteNonQuery();
+            if (rowsEffected > 0)
+            {
+                messageLabel.Text = "Student Information Saved Successfully";
+            }
+            //Connection Close
+            sqlConnection.Close();
+        }
+    }
+}
